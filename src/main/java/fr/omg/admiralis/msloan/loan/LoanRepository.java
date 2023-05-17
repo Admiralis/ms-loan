6e515eb0fb232c19baa5e395bf14d600b1a832ec
0h My Goat <daftouney@gmail.com>
2023-05-17 11:59:49 +0200

    fix: 500 on swagger :bug:

--- src/main/java/fr/omg/admiralis/msloan/loan/LoanRepository.java
+++ src/main/java/fr/omg/admiralis/msloan/loan/LoanRepository.java
@@ -3,17 +3,18 @@ package fr.omg.admiralis.msloan.loan;
 import fr.omg.admiralis.msloan.loan.model.Loan;
 import fr.omg.admiralis.msloan.loan.model.LoanStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface LoanRepository extends MongoRepository<Loan, String> {
-    List<Loan> findByComputerId(String computerId);
+    List<Loan> findByComputerId(@Param("computerId")String computerId);
 
-    List<Loan> findByStudentId(String studentId);
+    List<Loan> findByStudentId(@Param("studentId")String studentId);
 
-    List<Loan> findByCourseId(String courseId);
+    List<Loan> findByCourseId(@Param("courseId")String courseId);
 
-    List<Loan> findByCourseIdAndLoanStatus(String courseId, LoanStatus status);
+    List<Loan> findByCourseIdAndLoanStatus(@Param("courseId")String courseId, @Param("status")LoanStatus status);
 
-    List<Loan> findByComputerIdAndLoanStatus(String computerId, LoanStatus status);
+    List<Loan> findByComputerIdAndLoanStatus(@Param("computerId")String computerId, @Param("status")LoanStatus status);
 }
